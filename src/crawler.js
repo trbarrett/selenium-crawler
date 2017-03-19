58d02dac52835fe5cd7c36b656a281308d2d278d
Tristian Barrett <trbarrett@gmail.com>
2017-03-19 20:24:55 +0000

    More crawler refactoring

--- src/crawler.js
+++ src/crawler.js
@@ -2,62 +2,65 @@ const startBreadthFirstSearch = require('./breadth_first_search.js');
 const getPageLinksAndAssets = require('./get_page_links_and_assets.js');
 
 function crawlDomain(startUrl, domain, maxPages) {
-
   const startPage = { url: startUrl, assets: [] };
 
   const crawlerIter = startBreadthFirstSearch(startPage,
     page => page.url //key is the url
   );
 
-  // returns a promise, which will crawl all the pages in crawlerIter up to the
-  // maxPages count
-  return crawlPage(
-    [], //the accumulator for our reducer
-    crawlerIter,
-    crawlerIter.next().value,
-    maxPages,
-    0);
+  // returns a promise, which will crawl all the pages
+  // in crawlerIter up to the maxPages count
+  return crawlAll(crawlerIter, maxPages);
 }
-
 
 
 /**
  * Returns a promise which will iterate through all the pages,
  * and end up returning the list of crawled pages when finished
  * It's basically a reducer function on an potentially infintie
- * generator function
+ * generator function: crawlerIter
  *
  */
-function crawlPage(
-    crawledPages, //accumulator
-    crawlerIter, //breadth-first-search generator function 
-    page,        //current page
-    maxPages,
-    visitedPagesCount) {
+function crawlAll(crawlerIter, maxPages) {
+  const crawledPages = [];
+  const crawler = (function(page) {
+    //crawl the given page
+    return crawlPage(page).then(linkedPages => {
+      crawledPages.push(page);
 
-  //we've hit the limit of the pages we want to visit
-  if (visitedPagesCount >= maxPages) {
-    return Promise.resolve(crawledPages);
-  }
-  
-  //We've run out of pages to visit
-  if (!page) {
-    return Promise.resolve(crawledPages);
-  }
+      //we've hit the limit of the pages we want to visit
+      if (crawledPages.length >= maxPages) {
+        return crawledPages;
+      }
 
+      const nextPage = crawlerIter.next(linkedPages).value; 
+
+      //We've run out of pages to visit
+      if (!nextPage) {
+        return crawledPages;
+      }
+
+      //continue crawling
+      return crawler(nextPage);
+    });
+  });
+
+  return crawler(crawlerIter.next().value);
+}
+
+
+/**
+ * Returns a promise, that will load all the links and assets for
+ * the page, set the asssets on the page object, and return the
+ * resolve to the linked pages if any
+ */
+function crawlPage(page) {
   return getPageLinksAndAssets(page.url).then(([links, assets]) => {
-    page.assets = assets;
-    // map each link into an actual page object that we want to visit next
+    page.assets = assets; //set the assest for the page
+    // map each link into an actual page object that we want to visit
+    // in the future
     const linkedPages = links.map(l => ({ url: l, assets: [] }));
-
-    return crawlPage(
-      crawledPages.concat(page),
-      crawlerIter,
-      // we pass the links back to the crawler so it can continue crawling
-      crawlerIter.next(linkedPages).value, 
-      maxPages,
-      visitedPagesCount + 1
-    );
+    return linkedPages;
   });
 }
 
